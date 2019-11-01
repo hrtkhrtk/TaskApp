64559c16a3e05326eace859b6368d57f72806bb2
hirotaka.iwasaki <nddgs@yahoo.co.jp>
2019-11-01 14:46:40 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/hirotaka/iwasaki/taskapp/TaskApp.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.hirotaka.iwasaki.taskapp
+
+import android.app.Application
+import io.realm.Realm
+
+class TaskApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+    }
+}
